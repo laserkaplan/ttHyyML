d08685d213930975a00ac5214b5535a1c3ced184
Laser Seymour Kaplan <laserkaplan@gmail.com>
2017-04-09 15:48:11 +0200

    Split into binary and categorical models

--- ttHyy/models.py
+++ ttHyy/models.py
@@ -1,16 +1,20 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 
-def model_shallow(ndim, nclasses, doDropout=False):
+def model_shallow(ndim, doDropout=False):
     model = Sequential()
     model.add(Dense(16, input_dim = ndim))
     model.add(Activation('relu'))
-    if doDropout:
-        model.add(Dropout(0.2))
-    if nclasses == 2:
-        model.add(Dense(1))
-        model.add(Activation('sigmoid'))
-    else: 
-        model.add(Dense(nclasses))
-        model.add(Activation('softmax'))
+    if doDropout: model.add(Dropout(0.2))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    return model
+
+def model_shallow_categorical(ndim, nclasses, doDropout=False):
+    model = Sequential()
+    model.add(Dense(16, input_dim = ndim))
+    model.add(Activation('relu'))
+    if doDropout: model.add(Dropout(0.2))
+    model.add(Dense(nclasses))
+    model.add(Activation('softmax'))
     return model
