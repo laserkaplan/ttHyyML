6380d12e0911d7dd9f418f116087137c1417ec77
Laser Seymour Kaplan <laserkaplan@gmail.com>
2017-04-12 14:03:42 +0200

    Add hadronic model training

--- train.py
+++ train.py
@@ -122,6 +122,90 @@ def train_leptonic():
 
     return
 
+def train_hadronic():
+    # load data
+    print('Loading data.')
+
+    branches = ['N_j_30', 'N_j_central30', 'N_j_btag30', 'm_HT_30/1000', 'm_alljet/1000']
+    selectionMC   = 'N_lep == 0 && N_j_30 >= 3 && N_j_btag30 > 0'
+    selectiondata = 'N_lep == 0 && N_j_30 >= 3 && N_j_btag30 > 0 && (ph_isTight1 == 0 || ph_iso1 == 0 || ph_isTight2 == 0 || ph_iso2 == 0)'
+
+    sig      = root2array('inputs_hadronic/ttH.root'             , treename='output;14', branches=branches, selection=selectionMC  )
+    bkg_data = root2array('inputs_hadronic/data_LooseLepton.root', treename='output'   , branches=branches, selection=selectiondata)
+    bkg_ggH  = root2array('inputs_hadronic/ggH.root'             , treename='output;8' , branches=branches, selection=selectionMC  )
+
+    bkg = np.concatenate((bkg_data, bkg_ggH))
+
+    sig = utils.restrictSample(sig, len(bkg), args.signal)
+
+    sig = rec2array(sig)
+    bkg = rec2array(bkg)
+
+    # split data into train, val, and test samples
+    print('Splitting data.')
+
+    train_sig, test_sig = model_selection.train_test_split(sig, test_size = 0.3, random_state=1234)
+    train_bkg, test_bkg = model_selection.train_test_split(bkg, test_size = 0.3, random_state=1234)
+    val_sig, test_sig = np.split(test_sig, [len(test_sig) / 2])
+    val_bkg, test_bkg = np.split(test_bkg, [len(test_bkg) / 2])
+
+    headers = ['Sample', 'Total', 'Training', 'Validation', 'Testing']
+    sample_size_table = [
+        ['Signal'    , len(sig), len(train_sig), len(val_sig), len(test_sig)],
+        ['Background', len(bkg), len(train_bkg), len(val_bkg), len(test_bkg)],
+    ]
+    print tabulate(sample_size_table, headers=headers, tablefmt='simple')
+
+    # organize data for training
+    print('Organizing data for training.')
+
+    train = np.concatenate((train_sig, train_bkg))
+    val   = np.concatenate((val_sig  , val_bkg  ))
+    test  = np.concatenate((test_sig , test_bkg ))
+
+    y_train_cat = np.concatenate((np.zeros(len(train_sig), dtype=np.uint8), np.ones(len(train_bkg), dtype=np.uint8)))
+    y_val_cat   = np.concatenate((np.zeros(len(val_sig)  , dtype=np.uint8), np.ones(len(val_bkg)  , dtype=np.uint8)))
+    y_test_cat  = np.concatenate((np.zeros(len(test_sig) , dtype=np.uint8), np.ones(len(test_bkg) , dtype=np.uint8)))
+
+    # train model
+    print('Train model.')
+
+    model = models.model_shallow(5, True)
+    model.summary()
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+    model.fit(train, y_train_cat, epochs=20, batch_size=32, validation_data=(val, y_val_cat))
+
+    # test model
+    print('Test model.')
+
+    score = model.predict(test, batch_size=32)
+
+    # plot ROC curve
+    print('Plotting ROC curve')
+
+    fpr, tpr, _ = roc_curve(y_test_cat, score)
+    roc_auc = auc(fpr, tpr)
+
+    plt.plot(fpr, tpr, label='Shallow NN, area = %0.2f' % roc_auc)
+    plt.plot([0, 1], [0, 1], linestyle='--', color='black', label='Luck')
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc='lower right')
+
+    pltname = 'plots/ROC_curve_hadronic_' + args.name
+    plt.savefig(pltname + '.png')
+    plt.savefig(pltname + '.eps')
+    
+    plt.show()
+
+    # save model
+    if args.save:
+        print('Saving model')
+        model.save_weights('models/model_hadronic_shallow.h5')
+
+    return
+
 def main():
     if args.channel[0] == 'l':
         if args.cat: 
@@ -131,8 +215,12 @@ def main():
             print('Training binary leptonic model.')
             train_leptonic()
     else:
-        print('No support for hadronic model yet!')
-        return
+        if args.cat: 
+            print('No support for categorical model yet!')
+            return
+        else:
+            print('Training binary hadronic model.')
+            train_hadronic()
 
     return
 
