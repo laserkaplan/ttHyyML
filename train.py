bed552b1343424272354382a96dce8c994c41d65
Laser Seymour Kaplan <laserkaplan@gmail.com>
2017-04-25 17:00:19 +0200

    Add leptonic categorical model

--- train.py
+++ train.py
@@ -66,16 +66,6 @@ def train_leptonic():
     val   = np.concatenate((val_sig  , val_bkg  ))
     test  = np.concatenate((test_sig , test_bkg ))
 
-    '''
-    y_train = np.concatenate((np.zeros(len(train_sig), dtype=np.uint8), np.ones(len(train_bkg), dtype=np.uint8)))
-    y_val   = np.concatenate((np.zeros(len(val_sig)  , dtype=np.uint8), np.ones(len(val_bkg)  , dtype=np.uint8)))
-    y_test  = np.concatenate((np.zeros(len(test_sig) , dtype=np.uint8), np.ones(len(test_bkg) , dtype=np.uint8)))
-
-    y_train_cat = to_categorical(y_train, 2)
-    y_val_cat   = to_categorical(y_val  , 2)
-    y_test_cat  = to_categorical(y_test , 2)
-    '''
-    
     y_train_cat = np.concatenate((np.zeros(len(train_sig), dtype=np.uint8), np.ones(len(train_bkg), dtype=np.uint8)))
     y_val_cat   = np.concatenate((np.zeros(len(val_sig)  , dtype=np.uint8), np.ones(len(val_bkg)  , dtype=np.uint8)))
     y_test_cat  = np.concatenate((np.zeros(len(test_sig) , dtype=np.uint8), np.ones(len(test_bkg) , dtype=np.uint8)))
@@ -94,7 +84,7 @@ def train_leptonic():
     score = model.predict(test, batch_size=32)
 
     # plot ROC curve
-    print('Plotting ROC curve')
+    print('Plotting ROC curve.')
 
     pltname = 'ROC_curve_leptonic_' + args.name
     utils.plotROC(y_test_cat, score, pltname, True)
@@ -103,6 +93,103 @@ def train_leptonic():
     if args.save:
         print('Saving model')
         model.save_weights('models/model_leptonic_shallow.h5')
+
+    return
+
+def train_leptonic_categorical():
+    # load data
+    print('Loading data.')
+
+    branches = ['N_j_central30', 'm_HT_30/1000', 'm_mT/1000', 'm_pTlepEtmiss/1000']
+    selectionMC   = 'N_lep > 0 && N_j_btag30 > 0'
+    selectiondata = 'N_lep > 0 && N_j_btag30 == 0 && N_j_central30 > 0 && (ph_isTight1 == 0 || ph_iso1 == 0 || ph_isTight2 == 0 || ph_iso2 == 0)'
+
+    ttH  = root2array('inputs_leptonic/ttHrw.root'        , treename='output;5', branches=branches, selection=selectionMC  )
+    tHjb = root2array('inputs_leptonic/tHjbrw.root'       , treename='output'  , branches=branches, selection=selectionMC  )
+    tWH  = root2array('inputs_leptonic/tWHrw.root'        , treename='output'  , branches=branches, selection=selectionMC  )
+    bkg  = root2array('inputs_leptonic/data_looserw.root' , treename='output'  , branches=branches, selection=selectiondata)
+
+    tH = np.concatenate((tHjb, tWH))
+
+    leastevents = min(len(ttH), len(tH), len(bkg))
+
+    ttH = utils.restrictSample(ttH, leastevents, args.signal)
+    tH  = utils.restrictSample(tH , leastevents, args.signal)
+    bkg = utils.restrictSample(bkg, leastevents, args.signal)
+
+    ttH = rec2array(ttH)
+    tH  = rec2array(tH )
+    bkg = rec2array(bkg)
+
+    # split data into train, val, and test samples
+    print('Splitting data.')
+
+    train_ttH, test_ttH = model_selection.train_test_split(ttH, test_size = 0.3, random_state=1234)
+    train_tH , test_tH  = model_selection.train_test_split(tH , test_size = 0.3, random_state=1234)
+    train_bkg, test_bkg = model_selection.train_test_split(bkg, test_size = 0.3, random_state=1234)
+    val_ttH, test_ttH = np.split(test_ttH, [len(test_ttH) / 2])
+    val_tH , test_tH  = np.split(test_tH , [len(test_tH ) / 2])
+    val_bkg, test_bkg = np.split(test_bkg, [len(test_bkg) / 2])
+
+    headers = ['Sample', 'Total', 'Training', 'Validation', 'Testing']
+    sample_size_table = [
+        ['ttH'       , len(ttH), len(train_ttH), len(val_ttH), len(test_ttH)],
+        ['tH'        , len(tH ), len(train_tH ), len(val_tH ), len(test_tH )],
+        ['Background', len(bkg), len(train_bkg), len(val_bkg), len(test_bkg)],
+    ]
+    print tabulate(sample_size_table, headers=headers, tablefmt='simple')
+
+    # organize data for training
+    print('Organizing data for training.')
+
+    train = np.concatenate((train_ttH, train_tH, train_bkg))
+    val   = np.concatenate((val_ttH  , val_tH  , val_bkg  ))
+    test  = np.concatenate((test_ttH , test_tH , test_bkg ))
+
+    y_train = np.concatenate((
+        np.zeros(len(train_ttH), dtype=np.uint8),
+        np.ones(len(train_tH), dtype=np.uint8),
+        np.ones(len(train_bkg), dtype=np.uint8) + 1,
+    ))
+    y_val = np.concatenate((
+        np.zeros(len(val_ttH), dtype=np.uint8),
+        np.ones(len(val_tH), dtype=np.uint8),
+        np.ones(len(val_bkg), dtype=np.uint8) + 1,
+    ))
+    y_test = np.concatenate((
+        np.zeros(len(test_ttH), dtype=np.uint8),
+        np.ones(len(test_tH), dtype=np.uint8),
+        np.ones(len(test_bkg), dtype=np.uint8) + 1,
+    ))
+
+    y_train_cat = to_categorical(y_train, 3)
+    y_val_cat   = to_categorical(y_val  , 3)
+    y_test_cat  = to_categorical(y_test , 3)
+    
+    # train model
+    print('Train model.')
+
+    model = models.model_shallow_categorical(4, 3, True)
+    model.summary()
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(train, y_train_cat, epochs=20, batch_size=32, validation_data=(val, y_val_cat))
+
+    # test model
+    print('Test model.')
+
+    score = model.predict_classes(test, batch_size=32)
+
+    # plot confusion matrix
+    print('Plotting confusion matrix.')
+
+    pltname = 'CM_leptonic_' + args.name
+    classes = ['ttH', 'tH', 'Background']
+    utils.plotCM(y_test, score, classes, pltname, True)
+
+    # save model
+    if args.save:
+        print('Saving model')
+        model.save_weights('models/model_leptonic_shallow_categorical.h5')
 
     return
 
@@ -165,7 +252,7 @@ def train_hadronic():
     score = model.predict(test, batch_size=32)
 
     # plot ROC curve
-    print('Plotting ROC curve')
+    print('Plotting ROC curve.')
 
     pltname = 'ROC_curve_hadronic_' + args.name
     utils.plotROC(y_test_cat, score, pltname, True)
@@ -180,8 +267,8 @@ def train_hadronic():
 def main():
     if args.channel[0] == 'l':
         if args.cat: 
-            print('No support for categorical model yet!')
-            return
+            print('Training categorical leptonic model.')
+            train_leptonic_categorical()
         else:
             print('Training binary leptonic model.')
             train_leptonic()
--- ttHyy/utils.py
+++ ttHyy/utils.py
@@ -4,9 +4,11 @@ import ROOT
 
 import numpy as np
 
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_curve, auc, confusion_matrix
 
 import matplotlib.pyplot as plt
+
+import itertools
 
 def restrictSample(sample, nbkg, signal):
     '''
@@ -40,6 +42,37 @@ def plotROC(y_test, score, filename, show=False):
     plt.savefig('plots/' + filename + '.png')
     plt.savefig('plots/' + filename + '.eps')
     
+    if show: plt.show()
+
+    return
+
+def plotCM(y_test, score, classes, filename, show=False):
+    cm = confusion_matrix(y_test, score)
+    diag = float(np.trace(cm)) / float(np.sum(cm))
+    cm = cm.T.astype('float') / cm.T.sum(axis=0)
+    cm = np.rot90(cm.T, 1)
+    
+    np.set_printoptions(precision=2)
+
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title('Confusion matrix, diagonal = %.2f' % (100 * diag))
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes)
+    plt.yticks(tick_marks, classes[::-1])
+    
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, '%.2f' % (100 * cm[i, j]),
+                 horizontalalignment='center',
+                 color='white' if cm[i, j] > 100 * thresh else 'black')
+
+    plt.xlabel('True event label')
+    plt.ylabel('Event classification')
+    plt.tight_layout()
+
+    plt.savefig('plots/' + filename + '.png')
+    plt.savefig('plots/' + filename + '.eps')
+
     if show: plt.show()
 
     return
