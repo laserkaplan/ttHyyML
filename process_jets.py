e657c20dfaec6d1df119327bbe8803ab40860cbb
Laser Seymour Kaplan <laserkaplan@gmail.com>
2017-06-30 13:08:57 +0200

    Update process_jets for lepton

--- process_jets.py
+++ process_jets.py
@@ -19,6 +19,7 @@ def main():
 
     N_lep = array('i', [0])
     N_jet30 = array('i', [0])
+    N_jet30_cen = array('i', [0])
     N_bjet30_fixed70 = array('i', [0])
     flag_passedIso = array('i', [0])
     flag_passedPID = array('i', [0])
@@ -34,6 +35,7 @@ def main():
 
     t.SetBranchAddress('N_lep', N_lep)
     t.SetBranchAddress('N_jet30', N_jet30)
+    t.SetBranchAddress('N_jet30_cen', N_jet30_cen)
     t.SetBranchAddress('N_bjet30_fixed70', N_bjet30_fixed70)
     t.SetBranchAddress('flag_passedIso', flag_passedIso)
     t.SetBranchAddress('flag_passedPID', flag_passedPID)
@@ -53,23 +55,40 @@ def main():
         if (i % 10000 == 0): print('Event %d/%d' % (i, t.GetEntries()))
         t.GetEntry(i)
         if args.channel[0] == 'l':
-            if not (N_lep[0] > 0 and N_bjet30_fixed70[0] > 0): continue
+            if args.name[0:4] == 'data':
+                if not (N_lep[0] > 0 and N_bjet30_fixed70[0] == 0 and N_jet30_cen[0] > 0): continue
+            else:
+                if not (N_lep[0] > 0 and N_bjet30_fixed70[0] > 0): continue
+            if (args.data and flag_passedIso[0] and flag_passedPID[0]): continue
+            ej = []
+            if (len(j1) > 0): ej.append([j1[0], j1[1], j1[2], j1[3]])
+            if (len(j2) > 0): ej.append([j2[0], j2[1], j2[2], j2[3]])
+            if (len(j3) > 0): ej.append([j3[0], j3[1], j3[2], j3[3]])
+            if (len(j4) > 0): ej.append([j4[0], j4[1], j4[2], j4[3]])
+            if (len(j5) > 0): ej.append([j5[0], j5[1], j5[2], j5[3]])
+            if (len(j6) > 0): ej.append([j6[0], j6[1], j6[2], j6[3]])
+            if (len(j7) > 0): ej.append([j7[0], j7[1], j7[2], j7[3]])
+            ej = sorted(ej, key=lambda x: x[0], reverse=True)
+            for j in range(7 - len(ej)): ej.append([-999] * 4)
+            jets.append(ej)
+            pass
         else:
             if not (N_lep[0] == 0 and N_jet30[0] >= 3 and N_bjet30_fixed70[0] > 0): continue
-        if (args.data and flag_passedIso[0] and flag_passedPID[0]): continue
-        ej = []
-        if (len(j1) > 0): ej.append([j1[0], j1[1], j1[2], j1[3], j1[4]])
-        if (len(j2) > 0): ej.append([j2[0], j2[1], j2[2], j2[3], j2[4]])
-        if (len(j3) > 0): ej.append([j3[0], j3[1], j3[2], j3[3], j3[4]])
-        if (len(j4) > 0): ej.append([j4[0], j4[1], j4[2], j4[3], j4[4]])
-        if (len(j5) > 0): ej.append([j5[0], j5[1], j5[2], j5[3], j5[4]])
-        if (len(j6) > 0): ej.append([j6[0], j6[1], j6[2], j6[3], j6[4]])
-        if (len(j7) > 0): ej.append([j7[0], j7[1], j7[2], j7[3], j7[4]])
-        if (len(j8) > 0): ej.append([j8[0], j8[1], j8[2], j8[3], j8[4]])
-        if (len(j9) > 0): ej.append([j9[0], j9[1], j9[2], j9[3], j9[4]])
-        ej = sorted(ej, key=lambda x: x[4], reverse=True)
-        for j in range(9 - len(ej)): ej.append([-999] * 5)
-        jets.append(ej)
+            if (args.data and flag_passedIso[0] and flag_passedPID[0]): continue
+            ej = []
+            if (len(j1) > 0): ej.append([j1[0], j1[1], j1[2], j1[3], j1[4]])
+            if (len(j2) > 0): ej.append([j2[0], j2[1], j2[2], j2[3], j2[4]])
+            if (len(j3) > 0): ej.append([j3[0], j3[1], j3[2], j3[3], j3[4]])
+            if (len(j4) > 0): ej.append([j4[0], j4[1], j4[2], j4[3], j4[4]])
+            if (len(j5) > 0): ej.append([j5[0], j5[1], j5[2], j5[3], j5[4]])
+            if (len(j6) > 0): ej.append([j6[0], j6[1], j6[2], j6[3], j6[4]])
+            if (len(j7) > 0): ej.append([j7[0], j7[1], j7[2], j7[3], j7[4]])
+            if (len(j8) > 0): ej.append([j8[0], j8[1], j8[2], j8[3], j8[4]])
+            if (len(j9) > 0): ej.append([j9[0], j9[1], j9[2], j9[3], j9[4]])
+            ej = sorted(ej, key=lambda x: x[4], reverse=True)
+            for j in range(9 - len(ej)): ej.append([-999] * 5)
+            jets.append(ej)
+            pass
         pass
 
     jets = np.array(jets)
