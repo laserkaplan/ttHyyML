a9002bdce4713d6ca3ff8a59a9dcead86aee1f40
Laser Seymour Kaplan <laserkaplan@gmail.com>
2017-07-07 11:16:22 +0200

    Update applyWeight_rnn for leptonic

--- applyWeight_rnn.py
+++ applyWeight_rnn.py
@@ -2,6 +2,7 @@ import os
 
 import argparse
 parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--channel', action='store', choices=['l', 'lep', 'leptonic', 'h', 'had', 'hadronic'], default='l', help='Channel to process')
 parser.add_argument('-i', '--input', action='store', required=True, help='Name of input ROOT file to apply weights to')
 parser.add_argument('-n', '--name', action='store', required=True, help='Name to be appended to output file') 
 args = parser.parse_args()
@@ -21,31 +22,115 @@ from sklearn.preprocessing import StandardScaler
 
 from keras import models
 
-def process_photons(photons):
-    ptts = []
+def process_leptonic():
+    # load model
+    model = models.load_model('models/model_leptonic_rnn_temp.h5')
+    
+    # open file and get tree
+    infile = ROOT.TFile.Open('inputs/%s_RNN.root' % args.input)
+    intree = infile.Get('output')
 
-    for p in photons:
-        tlv1 = ROOT.TLorentzVector()
-        tlv1.SetPtEtaPhiE(p['photon_1'][0], p['photon_1'][1], p['photon_1'][2], p['photon_1'][3])
-        tlv2 = ROOT.TLorentzVector()
-        tlv2.SetPtEtaPhiE(p['photon_2'][0], p['photon_2'][1], p['photon_2'][2], p['photon_2'][3])
-        ptts.append(math.fabs(tlv1.Px() * tlv2.Py() - tlv1.Py() * tlv2.Px()) / (tlv1 - tlv2).Pt() * 2.0)
-        pass
+    in_mass_yy = array('f', [0])
+    in_weight = array('f', [0])
+    in_N_lep = array('i', [0])
+    in_N_jet30_cen = array('i', [0])
+    in_N_bjet30_fixed70 = array('i', [0])
+    in_flag_passedIso = array('i', [0])
+    in_flag_passedPID = array('i', [0])
 
-    return np.array(ptts)
+    intree.SetBranchAddress('mass_yy', in_mass_yy)
+    intree.SetBranchAddress('weight', in_weight)
+    intree.SetBranchAddress('N_lep', in_N_lep)
+    intree.SetBranchAddress('N_jet30_cen', in_N_jet30_cen)
+    intree.SetBranchAddress('N_bjet30_fixed70', in_N_bjet30_fixed70)
+    intree.SetBranchAddress('flag_passedIso', in_flag_passedIso)
+    intree.SetBranchAddress('flag_passedPID', in_flag_passedPID)
+
+    # create new tree
+    outtree = ROOT.TTree(args.name, args.name)
+    outtree.SetDirectory(0)
+
+    # create branches for tree
+    out_mass_yy = array('f', [0])
+    out_weight = array('f', [0])
+    out_flag_passedIso = array('i', [0])
+    out_flag_passedPID = array('i', [0])
+    out_nnweight = array('f', [0])
+    outtree.Branch('mass_yy', out_mass_yy, 'mass_yy/F')
+    outtree.Branch('weight', out_weight, 'weight/F')
+    outtree.Branch('flag_passedIso', out_flag_passedIso, 'flag_passedIso/I')
+    outtree.Branch('flag_passedPID', out_flag_passedPID, 'flag_passedPID/I')
+    outtree.Branch('nnweight', out_nnweight, 'nnweight/F')
+
+    jets = np.load('arrays/%s_leptonic_jets.npy' % args.input)
+    photons = np.load('arrays/%s_leptonic_photons.npy' % args.input)
+    lepmets = np.load('arrays/%s_leptonic_lepmets.npy' % args.input)
+    
+    scalers = pickle.load(open('scalers_leptonic.p', 'rb'))
+    
+    pts = jets[:, :, 0].flatten()
+    mask = pts != -999
+    pts[mask] = scalers['pt'].transform(pts[mask])
+    jets[:, :, 0] = pts.reshape(-1, 7)
+    
+    Es = jets[:, :, 3].flatten()
+    Es[mask] = scalers['E'].transform(Es[mask])
+    jets[:, :, 3] = Es.reshape(-1, 7)
+    
+    pts = photons[:, :, 0].flatten()
+    pts = scalers['pt'].transform(pts)
+    photons[:, :, 0] = pts.reshape(-1, 2)
+    
+    Es = photons[:, :, 3].flatten()
+    Es = scalers['E'].transform(Es)
+    photons[:, :, 3] = Es.reshape(-1, 2)
+
+    pts = lepmets[:, :, 0].flatten()
+    pts = scalers['pt'].transform(pts)
+    lepmets[:, :, 0] = pts.reshape(-1, 2)
+    
+    Es = lepmets[:, :, 3].flatten()
+    Es = scalers['E'].transform(Es)
+    lepmets[:, :, 3] = Es.reshape(-1, 2)
+
+    if args.input[0:4] == 'data':
+        if (intree.GetEntries('N_lep > 0 && N_bjet30_fixed70 == 0 && N_jet30_cen > 0 && mass_yy > 80000') != len(jets) or intree.GetEntries('N_lep > 0 && N_bjet30_fixed70 == 0 && N_jet30_cen > 0 && mass_yy > 80000') != len(photons) or intree.GetEntries('N_lep > 0 && N_bjet30_fixed70 == 0 && N_jet30_cen > 0 && mass_yy > 80000') != len(lepmets)):
+            print('Not the same number of events! Exiting!!!')
+            return
+    else:
+        if (intree.GetEntries('N_lep > 0 && N_bjet30_fixed70 > 0 && mass_yy > 80000') != len(jets) or intree.GetEntries('N_lep > 0 && N_bjet30_fixed70 > 0 && mass_yy > 80000') != len(photons) or intree.GetEntries('N_lep > 0 && N_bjet30_fixed70 > 0 && mass_yy > 80000') != len(lepmets)):
+            print('Not the same number of events! Exiting!!!')
+            return
+
+    events = np.concatenate((photons, lepmets, jets), axis=1)
+    score = model.predict(events)
+
+    j = 0
+    for i in range(intree.GetEntries()):
+        intree.GetEntry(i)
+        if args.input[0:4] == 'data':
+            if not (in_N_lep[0] > 0 and in_N_bjet30_fixed70[0] == 0 and in_N_jet30_cen[0] > 0 and in_mass_yy[0] > 80000): continue
+        else:
+            if not (in_N_lep[0] > 0 and in_N_bjet30_fixed70[0] > 0 and in_mass_yy[0] > 80000): continue
+        out_mass_yy[0] = in_mass_yy[0]
+        out_weight[0] = in_weight[0]
+        out_flag_passedIso[0] = in_flag_passedIso[0]
+        out_flag_passedPID[0] = in_flag_passedPID[0]
+        out_nnweight[0] = score[j]
+        outtree.Fill()
+        j += 1
+
+    # save new tree
+    outfile = ROOT.TFile('outputs/%s_leptonic_%s.root' % (args.input, args.name), 'RECREATE')
+    outtree.Write()
+    outfile.Close()
+
+    return
 
 def process_hadronic():
     # load model
-    model = models.load_model('models/model_hadronic_rnn_with_photons_temp.h5')
+    model = models.load_model('models/model_hadronic_rnn_temp.h5')
     
-    # get photons
-    branches = ['photon_1', 'photon_2']
-    selection   = 'N_lep == 0 && N_jet30 >= 3 && N_bjet30_fixed70 > 0'
-
-    photons = root2array('inputs/%s_RNN.root' % args.input, branches=branches, selection=selection)
-
-    ptts = process_photons(photons)
-
     # open file and get tree
     infile = ROOT.TFile.Open('inputs/%s_RNN.root' % args.input)
     intree = infile.Get('output')
@@ -83,8 +168,9 @@ def process_hadronic():
     outtree.Branch('nnweight', out_nnweight, 'nnweight/F')
 
     jets = np.load('arrays/%s_hadronic_jets.npy' % args.input)
+    photons = np.load('arrays/%s_hadronic_photons.npy' % args.input)
     
-    scalers = pickle.load(open('scalers.p', 'rb'))
+    scalers = pickle.load(open('scalers_hadronic.p', 'rb'))
     
     pts = jets[:, :, 0].flatten()
     mask = pts != -999
@@ -94,14 +180,22 @@ def process_hadronic():
     Es = jets[:, :, 3].flatten()
     Es[mask] = scalers['E'].transform(Es[mask])
     jets[:, :, 3] = Es.reshape(-1, 9)
+    
+    pts = photons[:, :, 0].flatten()
+    pts = scalers['pt'].transform(pts)
+    photons[:, :, 0] = pts.reshape(-1, 2)
+    
+    Es = photons[:, :, 3].flatten()
+    Es = scalers['E'].transform(Es)
+    photons[:, :, 3] = Es.reshape(-1, 2)
 
-    photons = scalers['ptt'].transform(ptts)
-
-    if (intree.GetEntries('N_lep == 0 && N_jet30 >= 3 && N_bjet30_fixed70 > 0') != len(jets) and intree.GetEntries('N_lep == 0 && N_jet30 >= 3 && N_bjet30_fixed70 > 0') != len(photons)):
+    if (intree.GetEntries('N_lep == 0 && N_jet30 >= 3 && N_bjet30_fixed70 > 0 && mass_yy > 80000') != len(jets) or intree.GetEntries('N_lep == 0 && N_jet30 >= 3 && N_bjet30_fixed70 > 0 && mass_yy > 80000') != len(photons)):
         print('Not the same number of events! Exiting!!!')
         return
 
-    score = model.predict([jets, photons])
+    photons = np.concatenate((photons, np.ones((photons.shape[0], photons.shape[1], 1))), axis=2)
+    events = np.concatenate((photons, jets), axis=1)
+    score = model.predict(events)
 
     j = 0
     for i in range(intree.GetEntries()):
@@ -122,18 +216,11 @@ def process_hadronic():
 
     return
 
-'''
 def main():
     if args.channel[0] == 'l':
         process_leptonic()
     else:
         process_hadronic()
-
-    return
-'''
-
-def main():
-    process_hadronic()
 
     return
 
